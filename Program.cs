09df8eab4adc14af31bd4783baab197d836a483d
priscilla glory <priscilla glory@LAPTOP-1L8F4TDI>
2023-12-26 10:39:43 +0530

    successfully added

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studentData
+{
+    internal class Program
+    {
+        private static void FileFun()
+        {
+            string path = @"C:\Users\priscilla glory\Desktop\PP2\studentData\studentData\studentDetails.txt";
+
+            if (File.Exists(path))
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    string tillEnd = sr.ReadToEnd();
+                    Console.WriteLine("File exists, reading in:\n" + tillEnd);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("Student Data\nRoll Number\tName");
+                    Console.WriteLine("File did not exist, created with default content.");
+                }
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            FileFun();
+        }
+    }
+}
